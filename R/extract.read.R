4bd39aa9cfaf00fd8871abad74596f178b18b07b
Rdataflow <rdataflow@users.noreply.github.com>
2017-02-28 22:05:09 +0100

    fix encoding

--- R/extract.read.R
+++ R/extract.read.R
@@ -1,4 +1,17 @@
-extract.read <- function(resource.local, type) {
-    dc <- as.data.table(read.px(resource.local))
+# reads files and returns datacube as data.table
+# file.input <- "/path/to/filename"
+# type <- "px" OR "csv" (datatype)
+extract.read <- function(file.input, type) {
+    if(type=="px") {
+        dc <- as.data.table(read.px(file.input, na.strings = c('"."','".."','"..."','"...."','"....."','"......"','":"')))
+        for (i in 1:(length(names(dc))-1)) {
+            levels(dc[[i]]) <- enc2native(levels(dc[[i]]))
+        }
+    }
+
+    if(type=="csv") {
+        dc <- fread(file.input)
+    }
+
     return(dc)
 }
